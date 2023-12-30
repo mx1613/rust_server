ca488797c3887b1c0416c1a04575734014666135
Michele <michele.xiloyannis@akina.health>
2023-12-30 17:57:06 +0100

    test: unit test for health check handler

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,7 @@
-
 pub struct ServerConfig {
     pub base_url: &'static str,
-
 }
 
-pub const SERVER_CONFIG: ServerConfig =  ServerConfig {
+pub const SERVER_CONFIG: ServerConfig = ServerConfig {
     base_url: "127.0.0.1:8000",
-
 };
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpResponse, HttpServer};
 mod config;
 use config::SERVER_CONFIG;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+
+    #[tokio::test]
+    async fn health_check_returns_ok() {
+        // Act
+        let response = health_check().await;
+        // Assert
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+}
 
 #[tokio::main] // this is the runtime
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(SERVER_CONFIG.base_url)?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind(SERVER_CONFIG.base_url)?
+        .run()
+        .await
 }
